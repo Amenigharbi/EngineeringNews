0cbb748477468d9d092a8dc2ecb339e7f2a77a9d
Amenigharbi <gharbiameni17@gmail.com>
2024-06-19 15:19:02 +0100

    updates

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-import express from 'express'
+import express, { RequestHandler } from 'express'
 const app=express();
 app.use(express.json())
 const posts:any[]= [];
+
+const requestLoggerMiddleware:RequestHandler=(req,res,next)=>{
+    console.log("new request",req.path,'body: ',req.body);
+    next();
+}
+app.use(requestLoggerMiddleware);
+app.use((req,res,next)=>{
+    console.log(Date.now());
+    next();
+});
 app.get('/posts',(request,response)=>{
     response.send({posts});
 })
